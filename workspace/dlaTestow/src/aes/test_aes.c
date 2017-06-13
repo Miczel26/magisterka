22c24d62c5bf73d78f3c8ed456ce81088eca4b6e
Michal Stroka <michal.stroka@globallogic.com>
2017-06-13 13:25:05 +0200

    Dodano testy aes crypt

--- workspace/dlaTestow/src/aes/test_aes.c
+++ workspace/dlaTestow/src/aes/test_aes.c
@@ -14,11 +14,13 @@
  */
 void test_aes_init(TIM_HandleTypeDef *timerHandle, Aes *aes, const byte *key, const byte *iv, uint8_t nrOfMeasurments)
 {
-
+    byte msg[16] = {0};
+    byte cipher[16];
         for (int i = 0; i < nrOfMeasurments; ++i)
         {
             TimerStart(timerHandle);
             wc_AesSetKey(aes, key, 16, iv, AES_ENCRYPTION);
+            wc_AesCbcEncrypt(aes, cipher, msg, 16);
             printf("%d\n", TimerStop(timerHandle));
         }
 }
